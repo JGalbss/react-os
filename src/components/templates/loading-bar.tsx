f1eadf56e8f982460b1ae8b4bc9ce0b8c8caa248
Josh Galbreath <joshsgalbreath@gmail.com>
2023-01-24 22:56:58 -0500

    Add progress bar

---
+++ src/components/templates/loading-bar.tsx
@@ -0,0 +1,9 @@
+const LoadingBar = () => {
+  return (
+    <div className="h-2.5 w-80 rounded-full bg-[#2D3549]">
+      <div className="h-2.5 rounded-full bg-white" style={{ width: '45%' }}></div>
+    </div>
+  );
+};
+
+export default LoadingBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import BackgroundGif from '@/public/images/background.gif';
+import LoadingBar from '@/components/templates/loading-bar';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  });
+
   return (
     <>
-     <div className='text-red-500'>Josh</div>
+      {!isLoading ? (
+        <div className="flex h-screen items-center justify-center bg-[#4258C6] bg-cover">
+          <div className="z-50">
+            <LoadingBar />
+          </div>
+          <div className="absolute bottom-0">
+            <Image alt="Background Image" src={BackgroundGif} />
+          </div>
+        </div>
+      ) : (
+        <div>Testing</div>
+      )}
     </>
-  )
+  );
 }
