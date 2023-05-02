8d32340d8a7dd1a6b4a8d2677a642130f710858d
Josh Galbreath <joshsgalbreath@gmail.com>
2023-05-02 16:25:09 -0400

    fix: test div remove

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,17 +67,17 @@ export default function Home() {
                   windowExpanded != '' ? 'z-10' : 'z-[100]',
                 )}
               />
-              <div className="z-[90] flex h-full w-full">
-                {APPS.map((app) => {
-                  return (
-                    <app.window
-                      windowSelected={windowSelected}
-                      setWindowSelected={setWindowSelected}
-                      classNames={clsx('z-50')}
-                    />
-                  );
-                })}
-              </div>
+
+              {APPS.map((app) => {
+                return (
+                  <app.window
+                    windowSelected={windowSelected}
+                    setWindowSelected={setWindowSelected}
+                    classNames={clsx('z-50')}
+                  />
+                );
+              })}
+
               <div className="absolute bottom-0 z-10">
                 <Image alt="Background Image" src={BackgroundGif} />
               </div>
